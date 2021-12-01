9a2fb5b1ec9b91a722082a13bd564cb4d99779ef
duonganh203 <rankill52@gmail.com>
2021-12-02 02:01:18 +0700

    Update team info

---
+++ assets/main.js
@@ -0,0 +1,51 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const myTeamInfo = [
+    {
+        name: 'Việt Nguyễn',
+        phoneNumber: '070270857',
+        role: 'Back End',
+        avatar: './assets/img/Eris-ln-thumb.jpg'
+    },
+    {
+        name: 'Dương Anh',
+        phoneNumber: '0584075164',
+        role: 'Front End',
+        avatar: './assets/img/Rudeus-ln-thumb.jpg'
+    },
+    {
+        name: 'Cường Nguyễn',
+        phoneNumber: '0775933761',
+        role: 'Dev Ops',
+        avatar: './assets/img/phong-canh-dep-trong-anime_112651330.jpg'
+    },
+]
+function render() {
+    myTeamInfo.forEach(function(data) {
+        const teamProfile = $('.team-profile')
+        const info = `
+        <div class="member">
+            <div class="member-content">
+                <div class="member-avatar">
+                    <img src="${data.avatar}" alt="">
+                </div>
+                <div class="member-info">
+                    <ul>
+                        <li>
+                            Full name: ${data.name}
+                        </li>
+                        <li>
+                            Phone Number: ${data.phoneNumber}
+                        </li>
+                        <li>
+                            Role: ${data.role}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `
+        teamProfile.innerHTML += info
+    })
+}
+render()
